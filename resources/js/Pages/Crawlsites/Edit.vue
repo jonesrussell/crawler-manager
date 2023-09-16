363b69864b79f61a371e822fe4106e0e5c55f775
Russell Jones <jonesrussell42@gmail.com>
2023-09-16 09:28:27 -0400

    Tags WIP

--- resources/js/Pages/Crawlsites/Edit.vue
+++ resources/js/Pages/Crawlsites/Edit.vue
@@ -5,6 +5,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Head, useForm } from "@inertiajs/vue3";
+import { ref, computed } from 'vue';
 
 const props = defineProps({
     Crawlsite: {
@@ -17,11 +18,31 @@ const form = useForm({
     id: props.Crawlsite.id,
     url: props.Crawlsite.url,
     content: props.Crawlsite.content,
+    tags: [],
 });
 
 
 const submit = () => {
     form.put(route("crawlsites.update", props.Crawlsite.id));
+};
+
+
+const newTag = ref('');
+const tagOptions = computed(() => []);
+const addTag = () => {
+    if (newTag.value.trim() === '') {
+        return;
+    }
+
+    if (!form.tags.includes(newTag.value)) {
+        form.tags.push(newTag.value);
+    }
+
+    newTag.value = '';
+};
+
+const removeTag = (tag) => {
+    form.tags = form.tags.filter((t) => t !== tag);
 };
 </script>
 
@@ -55,6 +76,25 @@ const submit = () => {
                         </form>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div>
+            <InputLabel for="tags" value="Tags" />
+            <div class="flex space-x-2 mt-1">
+                <!-- Input for adding new tags -->
+                <TextInput id="tags" type="text" class="block w-full" v-model="newTag" placeholder="Add tags..." />
+                <PrimaryButton @click="addTag">Add</PrimaryButton>
+            </div>
+            <div class="mt-2">
+                <!-- Display existing tags -->
+                <span v-for="(tag, index) in form.tags" :key="index"
+                    class="bg-gray-200 px-2 py-1 rounded-full text-sm text-gray-700 mr-2">
+                    {{ tag }}
+                    <button class="ml-1" @click="removeTag(tag)" type="button">
+                        Remove
+                    </button>
+                </span>
             </div>
         </div>
     </AuthenticatedLayout>
