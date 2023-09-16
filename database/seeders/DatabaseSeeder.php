f420c437f4c25c9376c1fdb24e5b619037bbe5e9
Russell Jones <jonesrussell42@gmail.com>
2023-09-16 15:45:57 -0400

    Seed a user and crawlsites

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create a "Russell Jones" user
+        User::factory()->create([
+            'name' => 'Russell Jones',
+            'email' => 'russell@example.com', // Change this to the desired email
+            'password' => bcrypt('password'),
+        ]);
+
+        // Uncomment the following lines if you want to create more users
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Seed other data using other seeders
+        $this->call([
+            CrawlsiteSeeder::class,
+        ]);
     }
 }
