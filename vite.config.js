206c36fcea9b409d6c2a30e9b3594355065db5e7
Russell Jones <jonesrussell42@gmail.com>
2023-10-02 11:13:39 -0400

    Add soketi; configure vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        // ... other server settings ...
+        host: '0.0.0.0', // Listen to all network requests
+        strictPort: true,
+        port: 5173, // Set the port to match the one exposed via DDEV
+        hmr: {
+            protocol: 'wss',
+            host: `${process.env.DDEV_HOSTNAME}`,
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
