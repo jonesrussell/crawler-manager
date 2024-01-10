86d222c1c330a08dd8a96b469b3eb38780fc04aa
Russell Jones <jonesrussell42@gmail.com>
2024-01-10 17:50:42 -0500

    Cleanup tasks views

--- resources/js/Pages/Crawlsites/Index.vue
+++ resources/js/Pages/Crawlsites/Index.vue
@@ -42,7 +42,6 @@ function destroy(id) {
                                 <thead
                                     class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
-                                        <th scope="col" class="px-6 py-3"></th>
                                         <th scope="col" class="px-6 py-3">Title</th>
                                         <th scope="col" class="px-6 py-3">URL</th>
                                         <!-- Add this th for the Search Terms -->
@@ -53,9 +52,6 @@ function destroy(id) {
                                 <tbody>
                                     <tr v-for="Crawlsite in crawlsites" :key="Crawlsite.id"
                                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                            {{ Crawlsite.id }}
-                                        </td>
                                         <td class="px-6 py-4">
                                             <Link :href="route('crawlsites.show', Crawlsite.id)">
                                             {{ Crawlsite.title }}
